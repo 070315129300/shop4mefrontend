ccb44b0a120b589f966c29e1a588c9ee03bd95ad
k-leb <ikhidecaleb@gmail.com>
2023-07-08 08:59:26 +0100

    fix whatapp

--- src/components/whatappwidgetCard.js
+++ src/components/whatappwidgetCard.js
@@ -1,19 +1,4 @@
-// import React from 'react';
-// import WhatsAppWidget from 'react-whatsapp-widget';
-// // import 'react-whatsapp-widget/dist/index.css';
 
-
-// const WhatsAppButton = () => {
-//   return (
-//     <WhatsAppWidget
-//       phoneNumber="1234567890" // Replace with your WhatsApp phone number
-//       companyName="My Company" // Replace with your company name
-//       message="Hello, I have a question." // Replace with your initial message
-//     />
-//   );
-// }
-
-// export default WhatsAppButton;
 
 import React from 'react';
 import { WhatsApp } from '@mui/icons-material';
