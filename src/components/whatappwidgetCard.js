3af6b4c1efee74c1c1412dd637fd27db1f051cb7
k-leb <ikhidecaleb@gmail.com>
2023-07-08 21:27:09 +0100

    fix whatapp

--- src/components/whatappwidgetCard.js
+++ src/components/whatappwidgetCard.js
@@ -1,20 +1,20 @@
 
 
 import React from 'react';
-// import { WhatsApp } from '@mui/icons-material';
+import { WhatsApp } from '@mui/icons-material';
 const WhatsAppButton = () => {
-//   const handleWhatsAppClick = () => {
-//     // Replace the URL with your WhatsApp API URL
-//     const apiURL = 'https://api.whatsapp.com/send?phone=08164520367&text=Hello,%20I%20have%20a%20question.';
+  const handleWhatsAppClick = () => {
+    // Replace the URL with your WhatsApp API URL
+    const apiURL = 'https://api.whatsapp.com/send?phone=08164520367&text=Hello,%20I%20have%20a%20question.';
 
-//     window.open(apiURL, '_blank');
-//   };
+    window.open(apiURL, '_blank');
+  };
 
-//   return (
-//     <div className="whatsapp-button" onClick={handleWhatsAppClick}>
-//           <WhatsApp className="whatsapp-icon" />
-//     </div>
-//   );
+  return (
+    <div className="whatsapp-button" onClick={handleWhatsAppClick}>
+          <WhatsApp className="whatsapp-icon" />
+    </div>
+  );
 }
 
 export default WhatsAppButton;
